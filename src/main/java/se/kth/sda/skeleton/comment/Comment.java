690d013371220b0299e8cd5a278ca9165df178bd
Elena Kolpakova <kolpakova.elena.p@gmail.com>
2020-11-24 14:11:43 +0100

    added Comment Entity

---
+++ src/main/java/se/kth/sda/skeleton/comment/Comment.java
@@ -0,0 +1,69 @@
+package se.kth.sda.skeleton.comment;
+
+import se.kth.sda.skeleton.task.Task;
+import se.kth.sda.skeleton.user.User;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "comment")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "body")
+    private String body;
+
+    //many comments from a single user
+    @ManyToOne
+    private User user;
+
+    //many comments to a singe task
+    @ManyToOne
+    private Task task;
+
+    public Comment() {
+    }
+
+    public Comment(Long id, String body, User user, Task task) {
+        this.id = id;
+        this.body = body;
+        this.user = user;
+        this.task = task;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Task getTask() {
+        return task;
+    }
+
+    public void setTask(Task task) {
+        this.task = task;
+    }
+}
---
+++ src/main/java/se/kth/sda/skeleton/task/Task.java
@@ -0,0 +1,2 @@
+package se.kth.sda.skeleton.task;public class Task {
+}
