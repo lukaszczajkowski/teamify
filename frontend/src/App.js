c1ec180b48795b921b26796e0db8dc601f7d84d0
Bhagyashri0312 <bhagyashri.laddha12@gmail.com>
2020-11-24 21:39:52 +0100

    Css for the login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import {
 } from "react-router-dom";
 
 // Import custom styles for our application
-import './App.css';
+import './css/style.css';
 
 import Auth from './services/Auth';
 import Navbar from "./components/layout/Navbar";
--- frontend/src/components/auth/LoginForm.js
+++ frontend/src/components/auth/LoginForm.js
@@ -5,40 +5,32 @@ function LoginForm({onSubmit}) {
     const [password, setPassword] = useState("");
 
     return (
-        <div className="card">
-            <div className="card-body">
-                <h4 className="card-title" >Login</h4>
-                <div>
-                    <div className="form-group">
-                        <label>Email:</label>
-                        <input 
-                            type="email"
-                            className="form-control"
-                            placeholder="Email"
-                            value={email}
-                            onChange={e => setEmail(e.target.value)} />
-                    </div>
+        <React.Fragment>
+            <label for='username'>Email: </label>
+                <input 
+                type="email"
+                className="form-control"
+                placeholder="Email"
+                value={email}
+                onChange={e => setEmail(e.target.value)} />
+                   
+            <label for='password'>Password:</label>
+                <input 
+                type="password" 
+                placeholder="Password" 
+                className="form-control" 
+                value={password}
+                onChange={e => setPassword(e.target.value)} />
+                    
 
-                    <div className="form-group">
-                        <label>Password:</label>
-                        <input 
-                            type="password" 
-                            placeholder="Password" 
-                            className="form-control" 
-                            value={password}
-                            onChange={e => setPassword(e.target.value)} />
-                    </div>
-
-                    <div className="form-group">
-                        <button 
-                            className="btn btn-info" 
-                            onClick={() => onSubmit({email, password})}>
-                            Login
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
+                    
+            <button 
+                className="submit" 
+                onClick={() => onSubmit({email, password})}>
+                    Login
+            </button>
+        </React.Fragment>
+            
     );
 }
 
