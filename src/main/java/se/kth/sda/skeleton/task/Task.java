5582e4dbe1a71d500d872bdc97b24029f38ca5bf
Elena Kolpakova <kolpakova.elena.p@gmail.com>
2020-11-24 14:13:34 +0100

    added tasks

--- src/main/java/se/kth/sda/skeleton/task/Task.java
+++ src/main/java/se/kth/sda/skeleton/task/Task.java
@@ -1,2 +1,75 @@
-package se.kth.sda.skeleton.task;public class Task {
+package se.kth.sda.skeleton.task;
+
+import se.kth.sda.skeleton.user.User;
+import se.kth.sda.skeleton.comment.Comment;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "task")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+
+    // Each task can have many users and each user can have many tasks
+    //@ManyToMany(mappedBy = "user")
+    @ManyToMany
+    private List<User> members;
+
+    // many comments to a singe task
+    @ManyToMany
+    private List<Comment> comments;
+
+    public Task() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<User> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<User> members) {
+        this.members = members;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
 }
