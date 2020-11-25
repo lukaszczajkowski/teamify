6363e89f1ac4b16841bc0f35f481db2b6ef8ada2
blacknstones <blacknstones@gmail.com>
2020-11-25 16:37:21 +0100

    created BeanBoard component

--- frontend/src/components/beans/BeanBoard.js
+++ frontend/src/components/beans/BeanBoard.js
@@ -1,9 +1,31 @@
 import React from "react";
 
+import BeanIcon from "../../assets/beanicon.png";
+
 export default function BeanBoard() {
-    return(
-        <div>
-            <h5>Your personal bean board</h5>
+    return (
+        <div className="bean-board board-container">
+            <div className="flex">
+                <img className="bean-icon" src={BeanIcon}></img>
+                <p>Your personal bean board</p>
+            </div>
+            <div className="flex">
+                <p>How are you feeling today? </p>
+                <p>Here is a slide</p>
+            </div>
+            <div className="flex">
+                <p>What is your goal for today? </p>
+                <p>Add new beans to your task list</p>
+            </div>
+            <div >
+                <button className="button">Drink water</button>
+                <button className="button">Exericise</button>
+                <button className="button">Meditate</button>
+                <button className="button">create a new bean</button>
+            </div>
+            <button className="button"><i className="fas fa-arrow-right"></i></button>
+
+
         </div>
     );
 }
