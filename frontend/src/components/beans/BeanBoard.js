ec623804566f2f7a8b43e70c8af489c373819876
blacknstones <blacknstones@gmail.com>
2020-11-25 17:52:20 +0100

    Modified user page

--- frontend/src/components/beans/BeanBoard.js
+++ frontend/src/components/beans/BeanBoard.js
@@ -5,25 +5,31 @@ import BeanIcon from "../../assets/beanicon.png";
 export default function BeanBoard() {
     return (
         <div className="bean-board board-container">
-            <div className="flex">
-                <img className="bean-icon" src={BeanIcon}></img>
-                <p>Your personal bean board</p>
+            <div className="flip-front">
+                <div className="flex">
+                    <img className="bean-icon" src={BeanIcon}></img>
+                    <p>Your personal bean board</p>
+                </div>
+                <div className="flex">
+                    <p>How are you feeling today? </p>
+                    <p>Here is a slide</p>
+                </div>
+                <div className="flex">
+                    <p>What is your goal for today? </p>
+                    <p>Add new beans to your task list</p>
+                </div>
+                <div >
+                    <button className="button">Drink water</button>
+                    <button className="button">Exericise</button>
+                    <button className="button">Meditate</button>
+                    <button className="button">create a new bean</button>
+                </div>
+                <button className="button"><i className="fas fa-arrow-right"></i></button>
             </div>
-            <div className="flex">
-                <p>How are you feeling today? </p>
-                <p>Here is a slide</p>
+
+            <div className="flip-back">
+
             </div>
-            <div className="flex">
-                <p>What is your goal for today? </p>
-                <p>Add new beans to your task list</p>
-            </div>
-            <div >
-                <button className="button">Drink water</button>
-                <button className="button">Exericise</button>
-                <button className="button">Meditate</button>
-                <button className="button">create a new bean</button>
-            </div>
-            <button className="button"><i className="fas fa-arrow-right"></i></button>
 
 
         </div>
