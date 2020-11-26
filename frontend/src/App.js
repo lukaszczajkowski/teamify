1843274beb65a4b25ab846f35490c06b61a62a99
blacknstones <blacknstones@gmail.com>
2020-11-26 19:51:41 +0100

    modified route for testing.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
-import React, {useState} from 'react';
-// import { useState} from 'react';
+import React, { useState } from 'react';
 import {
     BrowserRouter as Router,
     Switch,
@@ -17,10 +16,7 @@ import LandingPage from './components/home/LandingPage';
 import ProjectPage from "./components/projects/ProjectPage";
 import UserPage from "./components/user/UserPage";
 import ProjectBoard from './components/projects/ProjectBoard';
-import CreateTaskCard from './components/tasks/CreateTaskCard';
 import Auth from './services/Auth';
-//import RegisterForm from './components/auth/RegisterForm';
-//import LoginForm from './components/auth/LoginForm';
 
 function App() {
 
@@ -29,48 +25,56 @@ function App() {
 
     // eslint-disable-next-line no-unused-vars
     const [query, setQuery] = useState("");
-  
-  const loggedInRouter = (
-            <Router>
-                {/* <Navbar onLogout={() => Auth.logout()} /> */}
 
-                <div className="">
-                    <Switch>
-                        <Route path="/projects">
-                            <ProjectPage/>
-                        </Route>
+    const loggedInRouter = (
+        <Router>
+            {/* <Navbar onLogout={() => Auth.logout()} /> */}
 
-                        <Route path="/users">
-                            <UserPage/>
-                        </Route>
+            <div className="">
+                <Switch>
+                    <Route path="/projects">
+                        <ProjectPage />
+                    </Route>
 
-                        <Route path="/ProjectBoard">
-                            <ProjectBoard/>
-                        </Route>
+                    <Route path="/users">
+                        <UserPage />
+                    </Route>
 
-                        <Route path="/tasks">
-                          <CreateTaskCard/>
-                        </Route> 
+                    <Route path="/Projects">
+                        <ProjectPage />
+                    </Route>
 
-                    </Switch>
-                </div>
-            </Router>
-  );
+                </Switch>
+            </div>
+        </Router>
+    );
 
-  const notLoggedIn = (
-      <Router>
-          <Switch>
-              <Route exact path = "/">
-                <LandingPage/>
-              </Route>
-              <Route path = "/login">
-                  <LoginPage/>
-              </Route>
-          </Switch>
-      </Router>
-  )
+    const notLoggedIn = (
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <LandingPage />
+                </Route>
+                <Route path="/login">
+                    <LoginPage />
+                </Route>
 
-  return (loggedIn ? loggedInRouter : notLoggedIn);
+                <Route path="/projects">
+                    <ProjectPage />
+                </Route>
+
+                <Route path="/users">
+                    <UserPage />
+                </Route>
+
+                <Route path="/projects">
+                    <ProjectBoard />
+                </Route>
+            </Switch>
+        </Router>
+    )
+
+    return (loggedIn ? loggedInRouter : notLoggedIn);
 }
 
 export default App;
