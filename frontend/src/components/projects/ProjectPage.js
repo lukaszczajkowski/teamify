26a3977dca00c363bf948a08f48f074d75485325
blacknstones <blacknstones@gmail.com>
2020-11-29 19:33:20 +0100

    fixed project page redirect

--- frontend/src/components/projects/ProjectPage.js
+++ frontend/src/components/projects/ProjectPage.js
@@ -3,37 +3,58 @@ import Header from "../layout/Header";
 import CategoryApi from "../../api/CategoryApi";
 import CreateCategoryCard from "../categories/CreateCategoryCard";
 import CategoryCard from "../categories/CategoryCard";
+import { useParams, useHistory } from "react-router-dom";
+import ProjectApi from "../../api/ProjectApi";
 
 function ProjectPage() {
+    const history = useHistory();
+
+    const { projectId } = useParams();
     const [categories, setCategories] = useState([]);
 
+    const DeleteCurrentProject = () => {
+        return ProjectApi.deleteProject(projectId)
+            .then(history.push("/users"))
+            .catch(err => alert(`error on delete project ${err}`));
+    }
 
-    const getAll = () => {
-        CategoryApi.getAllCategories()
-            .then(response => setCategories(response.data));
+    const getAllCategories = () => {
+        return CategoryApi.getAllCategories()
+            .then(response => setCategories(response.data))
+            .catch(err => alert(`error on get all categories ${err}`));
     }
 
     const createCategory = (categoryData) => {
         CategoryApi.createCategory(categoryData)
-            .then(response => setCategories([response.data, ...categories]));
+            .then(response => setCategories([response.data, ...categories]))
+            .catch(err => alert(`error on create new category ${err}`));
     }
 
     useEffect(() => {
-        getAll()
+        getAllCategories()
     }, []);
 
     return (
         <div className="project-page">
             <Header />
-
+            <button className="button"
+                onClick={DeleteCurrentProject}>
+                Delete project
+                </button >
             <div className="projects-board flex-start">
-                    <div className="category-card card-container">category test</div>
-                    <div className="category-card card-container">category test</div>
+                <div className="category-card card-container">category test</div>
+                <div className="category-card card-container">category test</div>
 
-                {categories.map(category => (
-                    <CategoryCard key={category.id} category={category} />
-                ))}
-
+                {
+                    categories.length === 0 ?
+                        null :
+                        <div>
+                            {categories.map(category => (
+                                <CategoryCard key={category.id} category={category} />
+                            ))}
+                        </div>
+                }
+                
                 <div className="board-container">
                     <CreateCategoryCard onSubmit={createCategory} />
                 </div>
--- frontend/src/components/projects/ProjectsBoard.js
+++ frontend/src/components/projects/ProjectsBoard.js
@@ -5,23 +5,22 @@ import ProjectCard from "./ProjectCard";
 
 
 // eslint-disable-next-line react/prop-types
-export default function ProjectsBoard({projects, createProject}) {
+export default function ProjectsBoard({ projects, createProject }) {
 
     return (
         <div className="projects-board">
             <div className="board-container">
                 <p className="prompt">Keep working on projects:</p>
-               
-                <div className="project-cards flex-start">
-                <CreateProjectCard onSubmit={createProject} />
-                <Link to="/projects">
-                <button className="project-card">Testing card</button>
-                </Link>
-              
-                 {/*eslint-disable-next-line react/prop-types*/}
-                {projects.map(project => (
-                   <ProjectCard key={project.id} project={project}/>
-                ))}
+
+                <div className="project-cards flex-center flex-wrap">
+                    <CreateProjectCard onSubmit={createProject} />
+                    <Link to="/projects">
+                        <button className="project-card">Testing card</button>
+                    </Link>
+                        {/*eslint-disable-next-line react/prop-types*/}
+                        {projects.sort((a, b) => b.id - a.id).map(project => (
+                            <ProjectCard key={project.id} project={project} />
+                        ))}
                 </div>
             </div>
 
