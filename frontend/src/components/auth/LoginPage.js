32bc7b1410b80fdb05a085aab50c2d0f6c448bcb
Bhagyashri0312 <bhagyashri.laddha12@gmail.com>
2020-12-02 13:47:56 +0100

    small changes

--- frontend/src/components/auth/LoginPage.js
+++ frontend/src/components/auth/LoginPage.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import LoginForm from "./LoginForm";
 import RegisterForm from "./RegisterForm";
 import {useSpring, animated} from 'react-spring';
+import Header from "../layout/Header";
 
 
 function LoginPage() {
@@ -22,28 +23,25 @@ function LoginPage() {
     const loginBtnProps = useSpring({borderBottom: registrationFormStatus ? 'solid 0px transparent' : 'solid 2px #0F545C'})
     const registerBtnProps = useSpring({borderBottom: registrationFormStatus ? 'solid 2px #0F545C' : 'solid 0px transparent' })
 
-
-    
-
-    
-
     return (
-        <div className="login-register-wrapper">
+        <div>
+            <Header />
+            <div className="login-register-wrapper">
             <div className="nav-buttons">
                 <animated.button onClick={loginClicked} id="loginBtn" class='active' style= {loginBtnProps}>Login</animated.button>
                 <animated.button onClick={registerClicked} id="registrationBtn" style= {registerBtnProps}>Register</animated.button>
             </div>
             <div className="form-group">
-            <animated.form action='' id='loginform' style={loginProps}><LoginForm  /></animated.form>
-           <animated.form action='' id='registerform' style={registerProps}> <RegisterForm  /></animated.form>
+                <animated.form action='' id='loginform' style={loginProps}><LoginForm  /></animated.form>
+                <animated.form action='' id='registerform' style={registerProps}> <RegisterForm  /></animated.form>
             </div>
-            <animated.div className="forgot-panel" style={loginProps}>
-                <a href='#'> Forgot password? </a>
-            </animated.div>
-                
+                <animated.div className="forgot-panel" style={loginProps}>
+                    <a href='#'> Forgot password? </a>
+                </animated.div>
+            
             </div>
                    
-                            
+        </div>             
                                 
     );
 }
