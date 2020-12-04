7ed79feb8ae14987b89b41bdda4a6a38b4c48c7f
blacknstones <blacknstones@gmail.com>
2020-12-04 20:33:17 +0100

    Passed  creator to project components

--- frontend/src/components/projects/ProjectsBoard.js
+++ frontend/src/components/projects/ProjectsBoard.js
@@ -4,7 +4,7 @@ import ProjectCard from "./ProjectCard";
 
 
 // eslint-disable-next-line react/prop-types
-export default function ProjectsBoard({ projects, createProject }) {
+export default function ProjectsBoard({ creator, projects, createProject }) {
 
     return (
         <div className="projects-board">
@@ -12,7 +12,7 @@ export default function ProjectsBoard({ projects, createProject }) {
                 <p className="prompt">Keep working on projects:</p>
 
                 <div className="project-cards flex-center flex-wrap">
-                    <CreateProjectCard onSubmit={createProject} />
+                    <CreateProjectCard creator={creator} onSubmit={createProject} />
                 
                         {/*eslint-disable-next-line react/prop-types*/}
                         {projects.sort((a, b) => b.id - a.id).map(project => (
