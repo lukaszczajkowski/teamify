6c4e83f8d33b9a541d1bd9bfb88dcf284b436fa5
Bhagyashri0312 <bhagyashri.laddha12@gmail.com>
2020-12-04 09:42:13 +0100

    frontend add task minor changes

--- frontend/src/components/tasks/TaskCard.js
+++ frontend/src/components/tasks/TaskCard.js
@@ -1,25 +1,36 @@
 import React from "react";
+import {Paper} from '@material-ui/core';
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyle = makeStyles((theme) => ({
+    card: {
+        padding: theme.spacing(1,1,1,2),
+            margin: theme.spacing(1),
+          },
+}));
 
 // eslint-disable-next-line react/prop-types
-export default function TaskCard({ task, deleteTask }) {
-    const onDeleteTask = () => {
+export default function TaskCard({ task, /*deleteTask*/ }) {
+    const classes = useStyle();
+   // const onDeleteTask = () => {
         // eslint-disable-next-line react/prop-types
-        if (window.confirm(`Do you want to delete task ${task.id}?\n**Redesign this to a popup later**`)) {
+     //   if (window.confirm(`Do you want to delete task ${task.id}?\n**Redesign this to a popup later**`)) {
             // eslint-disable-next-line react/prop-types
-            deleteTask(task.id);
-        }
-    }
+         //   deleteTask(task.id);
+       // }
+  //  }
 
     return (
-        <div className="task-card flex-between">
+        <div >
             <div>
+
                 {/* eslint-disable-next-line react/prop-types*/}
-                <p>{task.title}</p>
+                <Paper className={classes.card}>{task.title}</Paper>
             </div>
 
-            <button className="button"
+          {/*  <button className="button"
                 id="delete-category"
-                onClick={onDeleteTask}>Delete</button>
+    onClick={onDeleteTask}>Delete</button> */}
         </div>
     );
 }
