32ec64a1c1b4ae3c1df8defda3fd95838243c571
blacknstones <blacknstones@gmail.com>
2020-12-06 19:51:45 +0100

    Added action buttons

--- frontend/src/components/tasks/TaskActions.js
+++ frontend/src/components/tasks/TaskActions.js
@@ -1,28 +1,27 @@
 import React, { useState } from "react";
 
 // eslint-disable-next-line react/prop-types
-export default function ProjectActions({onDeleteProject}) {
+export default function TaskActions({ onDeleteTask }) {
     const [click, setClick] = useState(false);
 
     const handleClick = () => setClick(!click);
 
     return (
-        <div className="action project-actions">
+        <div className="action category-actions">
             <button className="action-button" onClick={handleClick}>
-                <i className="fas fa-chevron-down"></i>
+                <i className="fas fa-ellipsis-v"></i>
             </button>
             {
                 click ?
                     <div className="action-menu">
-                        <button className="action-item"
-                            id="delete-project"
-                            onClick={onDeleteProject}>
-                            Delete project
-                </button>
+                        <button
+                            className="action-item"
+                            id="delete-task"
+                            onClick={onDeleteTask}>Delete Task
+                        </button>
                     </div> :
                     null
             }
         </div>
     );
 }
-
