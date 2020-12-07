f5fdab67250ec691e67693042e6d75b850b7719d
blacknstones <blacknstones@gmail.com>
2020-12-07 10:59:18 +0100

    modified project header

--- frontend/src/components/projects/MemberCard.js
+++ frontend/src/components/projects/MemberCard.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 // eslint-disable-next-line react/prop-types
 export default function MemberCard({member}) {
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+
     return (
-        <div className="member-card">
-            {/*eslint-disable-next-line react/prop-types*/}
-            <button className="member-button">{member.name.charAt(0)}</button>
+        <div className="member-card" onClick={handleClick}>
+          
+            <button className="member-button">  
+                    {/*eslint-disable-next-line react/prop-types*/}
+                        {member.name.charAt(0)}
+            </button>
+
+            {
+                click ?
+                    <div className="action-menu">
+                        <button className="action-item"
+                            id="delete-project">
+                            Delete member
+                </button>
+                    </div> :
+                    null
+            }
 
         </div>
     );
---
+++ frontend/src/components/projects/ProjectMenu.js
@@ -0,0 +1,37 @@
+import React from "react";
+import ProjectActions from "./ProjectActions";
+import MemberCard from "./MemberCard";
+import AddMemberPopup from "./AddMemberPopup";
+import MemberIcon from "../../assets/icon/member.png";
+
+// eslint-disable-next-line react/prop-types
+export default function ProjectMenu({ currentProject, members, onDeleteProject, addMemberByEmail }) {
+
+    return (
+        <div className="project-menu flex-start">
+            <div id="project-menu" className="flex-start">
+                {/* eslint-disable-next-line react/prop-types*/}
+                <p className="project-title">{currentProject.title}</p>
+                <ProjectActions onDeleteProject={onDeleteProject} />
+
+            </div>
+            <div id="member-menu" className="flex-start">
+            <img className="member-icon" src={MemberIcon} />
+                {
+                    members === null ?
+                        null :
+                        <div className="member-list flex-start">
+                            {/* eslint-disable-next-line react/prop-types*/}
+                            {members.map(member => (
+                                <MemberCard key={member.id}
+                                    member={member}
+                                />
+                            ))}
+
+                        </div>
+                }
+                <AddMemberPopup onSubmit={addMemberByEmail} />
+            </div>
+        </div>
+    );
+}
