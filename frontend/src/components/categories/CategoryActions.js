472fe4cf9be7ea2af75a94611888b446fd78d231
blacknstones <blacknstones@gmail.com>
2020-12-08 11:57:27 +0100

    upgraded action buttons

--- frontend/src/components/categories/CategoryActions.js
+++ frontend/src/components/categories/CategoryActions.js
@@ -1,27 +1,35 @@
-import React, { useState } from "react";
+import React from "react";
+import useComponentVisible from "../hooks/useComponentVisible";
 
 // eslint-disable-next-line react/prop-types
 export default function CategoryActions({ onDeleteCategory }) {
-    const [click, setClick] = useState(false);
+    const {
+        ref,
+        isComponentVisible,
+        setIsComponentVisible
+    } = useComponentVisible(false);
 
-    const handleClick = () => setClick(!click);
+    const handleClick = () => {
+        setIsComponentVisible(true);
+    }
 
     return (
         <div className="action category-actions">
             <button className="action-button" onClick={handleClick}>
                 <i className="fas fa-ellipsis-v"></i>
             </button>
-            {
-                click ?
+
+            <div ref={ref}>
+                {isComponentVisible && (
                     <div className="action-menu">
                         <button
                             className="action-item"
                             id="delete-category"
                             onClick={onDeleteCategory}>Delete List
                         </button>
-                    </div> :
-                    null
-            }
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
