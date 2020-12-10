dca5c4717c660063dd53ab44f7c8e4cbee408f48
Bhagyashri0312 <bhagyashri.laddha12@gmail.com>
2020-12-10 17:39:36 +0100

    frontend drag and drop

--- frontend/src/components/projects/ProjectBoard.js
+++ frontend/src/components/projects/ProjectBoard.js
@@ -4,10 +4,25 @@ import CategoryCard from "../categories/CategoryCard";
 import {DragDropContext} from 'react-beautiful-dnd';
 
 // eslint-disable-next-line react/prop-types
-export default function ProjectBoard( {projectId, categories, createCategory, updateCategory, deleteCategory }) {
+export default function ProjectBoard( {projectId, categories, createCategory, updateCategory, deleteCategory, category }) {
+    let onDragEnd = (result) => {
+        if (!result.destination) {
+        return;
+        }
+        let sourceIdx = parseInt(result.source.index)
+        let destIdx = parseInt(result.destination.index)
+         {/* eslint-disable-next-line react/prop-types */}
+        let draggedLink = category.task[sourceIdx]
+         {/* eslint-disable-next-line react/prop-types */}
+        let newList = category.task.slice();
+        newList.splice(sourceIdx, 1);
+        newList.splice(destIdx, 0, draggedLink)
+         {/* eslint-disable-next-line react/prop-types */}
+        category.task = newList;
+        }
     return (
         <div className="project-board flex-start">
-            <DragDropContext>
+            <DragDropContext onDragEnd = {onDragEnd}>
             
                 {
                 categories === null ?
