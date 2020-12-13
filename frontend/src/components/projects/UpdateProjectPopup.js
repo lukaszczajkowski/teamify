bcd522899a324de57ad64074afe7cbafefddc4a8
blacknstones <blacknstones@gmail.com>
2020-12-14 00:38:44 +0100

    refactored updateProjectPopup with EditableText

--- frontend/src/components/projects/UpdateProjectPopup.js
+++ frontend/src/components/projects/UpdateProjectPopup.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Popup from "reactjs-popup";
+import EditableText from "./EditableText";
 // import Popup from "reactjs-popup";
 
 // eslint-disable-next-line react/prop-types
@@ -8,42 +9,42 @@ export default function UpdateProjectPopup({ isOpen, currentProject, updateProje
     console.log("on updateProjectPopup. current project: " + currentProject.id);
 
     const {
-        // eslint-disable-next-line react/prop-types
         id, title, description,
         // users
     } = currentProject;
-
-    const [projectTitle, setProjectTitle] = useState(title);
-    const [projectDescription, setProjectDescription] = useState(description);
-    const [isEditingTitle, setIsEditingTitle] = useState(false);
-    const [isEditingDescription, setIsEditingDescription] = useState(false);
+   
 
     // const [taskMembers, setTaskMembers] = useState("");
     //const [taskComments, setTaskComments] = useState([]);
 
-    const updatedProject = {
-        id,
-        title: projectTitle,
-        description: projectDescription
+    const onTitleUpdated = (newTitle) => {
+        const updatedProject = {
+            id,
+            title: newTitle,
+            description,
+        };
+        updateProject(updatedProject);
     }
 
-    useEffect(() => {
-        setProjectTitle(title);
-    }, [currentProject]);
+    const onDescriptionUpdated = (newDescription) => {
+        const updatedProject = {
+            id,
+            title,
+            description: newDescription,
+        };
 
-    const onUpdateProject = () => {
         updateProject(updatedProject);
-        setIsEditingTitle(false);
-        setIsEditingDescription(false);
-    };
+    }
 
     return (
         <div className="task-popup popup-container">
             <Popup open={isOpen} modal nested onClose={onClose}>
                 <div className="modal">
+                    
                     <button className="close" onClick={onClose}>
                         <i className="fas fa-times"></i>
                     </button>
+
                     <div className="popup-header">
                         <h2 className="header-title">Edit project detail</h2>
 
@@ -51,49 +52,19 @@ export default function UpdateProjectPopup({ isOpen, currentProject, updateProje
                     <div className="content">
                         <div className="popup-item flex-start">
                             <h2 className="prompt">Title</h2>
-                            {
-                                isEditingTitle ?
-                                    <div className="title-input flex-between">
-                                        <input
-                                            type="text"
-                                            className="input-box"
-                                            placeholder="Title"
-                                            value={projectTitle}
-                                            onChange={e => setProjectTitle(e.target.value)}
-                                        />
-                                        <button
-                                            className="action-button" id="confirm-update"
-                                            onClick={onUpdateProject}>
-                                            <i className="fas fa-check"></i>
-                                        </button>
-                                    </div>
-                                    :
-
-                                    <p className="input-box" onClick={() => setIsEditingTitle(true)}>{projectTitle}</p>
-
-                            }
+                            <EditableText
+                                text={title}
+                                placeholder="title"
+                                onUpdateText={onTitleUpdated} />
                         </div>
+
                         <div className="popup-item flex-start">
                             <h2 className="prompt">Description</h2>
-                            {
-                                isEditingDescription ?
-                                    <div className="title-input flex-between">
-                                        <textarea
-                                           
-                                            className="input-box text-area"
-                                            placeholder="Enter description"
-                                            value={projectDescription}
-                                            onChange={e => setProjectDescription(e.target.value)}
-                                        />
-                                        <button
-                                            className="action-button" id="confirm-update"
-                                            onClick={onUpdateProject}>
-                                            <i className="fas fa-check"></i>
-                                        </button>
-                                    </div>
-                                    :
-                                    <p className="input-box" onClick={() => setIsEditingDescription(true)}>{projectDescription || "No description"}</p>
-                            }
+                            <EditableText
+                                text={description}
+                                placeholder="description"
+                                onUpdateText={onDescriptionUpdated}
+                            />
                         </div>
 
                     </div>
