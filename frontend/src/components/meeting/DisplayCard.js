b853f055bd1d1f73ee48e0a579dc7dda8b717b37
Elena Kolpakova <kolpakova.elena.p@gmail.com>
2020-12-15 21:22:37 +0100

    fix for unused Link

--- frontend/src/components/meeting/DisplayCard.js
+++ frontend/src/components/meeting/DisplayCard.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react"
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 import UserApi from "../../api/UserApi"
 import MeetingApi from "../../api/MeetingApi";
 
